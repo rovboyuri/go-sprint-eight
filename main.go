481b2f6a581caf9c66f46a77ec4bca2267bc632d
Yuri Rovbo <rovbo.yuv@gmail.com>
2025-02-10 21:29:52 +0300

    message

--- main.go
+++ main.go
@@ -97,9 +97,14 @@ func (s ParcelService) Delete(number int) error {
 }
 
 func main() {
-	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	store := NewParcelStore(db)
 	service := NewParcelService(store)
 
 	// регистрация посылки
