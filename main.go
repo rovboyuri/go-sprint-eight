775091c5e04af6332b5d9004cf07cdd560396ff7
Yuri Rovbo <rovbo.yuv@gmail.com>
2025-02-11 12:34:51 +0300

    second try

--- main.go
+++ main.go
@@ -104,6 +104,8 @@ func main() {
 		return
 	}
 
+	defer db.Close()
+
 	store := NewParcelStore(db)
 	service := NewParcelService(store)
 
--- parcel.go
+++ parcel.go
@@ -68,6 +68,12 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 		res = append(res, p)
 	}
 
+	err = rows.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
 	return res, nil
 }
 
